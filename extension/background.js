b75e0cf090377a2176ceb13637f09ca33f52ce69
Arushi <as72604@uga.edu>
2026-02-07 14:22:46 -0500

    Add break timer code

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,16 @@
 let studyActive = false;
 let studyTabId = null;
+
 let timer = null;
 let timeRemaining = 0;
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+let onBreak = false;
+let breakDuration = 300; // 5 minutes (you can change this)
+
+chrome.runtime.onMessage.addListener((msg, sender) => {
   if (msg.type === "START_STUDY") {
     studyActive = true;
+    onBreak = false;
     studyTabId = sender.tab.id;
     timeRemaining = msg.duration;
     startTimer();
@@ -13,34 +18,56 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 
   if (msg.type === "STOP_STUDY") {
     studyActive = false;
+    onBreak = false;
     clearInterval(timer);
   }
 });
 
 function startTimer() {
+  clearInterval(timer);
+
   timer = setInterval(() => {
     timeRemaining--;
 
     if (timeRemaining <= 0) {
       clearInterval(timer);
-      chrome.notifications.create({
-        type: "basic",
-        iconUrl: "icon.png",
-        title: "Study Session Complete",
-        message: "Time for a break! :)"
-      });
+
+      if (!onBreak) {
+        // Study session just ended → start break
+        onBreak = true;
+        timeRemaining = breakDuration;
+
+        chrome.notifications.create({
+          type: "basic",
+          iconUrl: "icon.png",
+          title: "Break Time!",
+          message: "Great work! Take a short break. :)"
+        });
+
+        startTimer();
+      } else {
+        // Break ended → notify user
+        onBreak = false;
+
+        chrome.notifications.create({
+          type: "basic",
+          iconUrl: "icon.png",
+          title: "Break Over",
+          message: "Ready for another study session?"
+        });
+      }
     }
   }, 1000);
 }
 
 // Detect tab switching
 chrome.tabs.onActivated.addListener(activeInfo => {
-  if (studyActive && activeInfo.tabId !== studyTabId) {
+  if (studyActive && !onBreak && activeInfo.tabId !== studyTabId) {
     chrome.notifications.create({
       type: "basic",
       iconUrl: "icon.png",
       title: "Stay Focused",
-      message: "You switched tabs. :( Let's stay on task and wait until a break to check other sites!"
+      message: "You switched tabs. :( Let's stay on task and wait for break time!"
     });
   }
 });
